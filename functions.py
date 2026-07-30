e1225fdb9b8b8570e517cb1c9714f49c52e306e5
srungarapusrivarsha-alt <srungarapusrivarsha@gmail.com>
2026-07-30 11:14:07 +0530

    functions scenario based

--- functions.py
+++ functions.py
@@ -1,67 +1,67 @@
-# def welcome():
-#     print("welcome ....")
-# welcome()
+def welcome():
+    print("welcome ....")
+welcome()
 
-# def add(a,b):
-#     print("sum is:",a+b)
-# num1=int(input("enter num 1:"))
-# num2=int(input("enter num 2:"))
-# add(num1,num2)
+def add(a,b):
+    print("sum is:",a+b)
+num1=int(input("enter num 1:"))
+num2=int(input("enter num 2:"))
+add(num1,num2)
 
-# def sub(a,b):
-#     print("diff is :",a-b)
-# num1=int(input("enter num1:"))
-# num2=int(input("enter num2:"))
-# sub(num1,num2)
+def sub(a,b):
+    print("diff is :",a-b)
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+sub(num1,num2)
 
-# def even(num):
-#     if num%2==0:
-#         print("even")
-#     else:
-#         print("odd")
-# i=int(input("enter number:--"))
-# even(i)
+def even(num):
+    if num%2==0:
+        print("even")
+    else:
+        print("odd")
+i=int(input("enter number:--"))
+even(i)
 
-# def area_rect(l,b):
-#     area=l*b
-#     print("area is :--",area)
-# l=int(input("enter number length:-")) 
-# b=int(input("enter number breadth:-"))
-# area_rect(l,b)   
+def area_rect(l,b):
+    area=l*b
+    print("area is :--",area)
+l=int(input("enter number length:-")) 
+b=int(input("enter number breadth:-"))
+area_rect(l,b)   
 
-# def simple_interest(p,t,r):
-#     interest=(p*t*r)/100
-#     print("simple interest is :-",interest)
-# p=float(input("enter principal:"))
-# t=float(input("enter time:"))
-# r=float(input("enter rate of interest:"))
-# simple_interest(p,t,r)
+def simple_interest(p,t,r):
+    interest=(p*t*r)/100
+    print("simple interest is :-",interest)
+p=float(input("enter principal:"))
+t=float(input("enter time:"))
+r=float(input("enter rate of interest:"))
+simple_interest(p,t,r)
 
-# def prime(num):
-#     if num<1:
-#         print("not prime")
-#         return
-#     for i in range(2,num):
-#         if num%i==0:
-#             print("not prime")
-#             return
-#     else:
-#         print("prime number")
-# num=int(input("enter a number:-"))
-# prime(num)
+def prime(num):
+    if num<1:
+        print("not prime")
+        return
+    for i in range(2,num):
+        if num%i==0:
+            print("not prime")
+            return
+    else:
+        print("prime number")
+num=int(input("enter a number:-"))
+prime(num)
 
-# def reverse(text):
-#     print("reverse:",text[::-1])
-# text=input("enter text:")
-# reverse(text)   
+def reverse(text):
+    print("reverse:",text[::-1])
+text=input("enter text:")
+reverse(text)   
 
-# def emp_sal(basic,bonus,tax):
-#     salary=basic+bonus-tax
-#     print("final salary:",salary)
-# basic=float(input("enter basic amount:"))
-# bonus=float(input("enter bonus salary:"))
-# tax=float(input(("enter tax paid:")))
-# emp_sal(basic,bonus,tax)    
+def emp_sal(basic,bonus,tax):
+    salary=basic+bonus-tax
+    print("final salary:",salary)
+basic=float(input("enter basic amount:"))
+bonus=float(input("enter bonus salary:"))
+tax=float(input(("enter tax paid:")))
+emp_sal(basic,bonus,tax)    
 
 
 students=[]
